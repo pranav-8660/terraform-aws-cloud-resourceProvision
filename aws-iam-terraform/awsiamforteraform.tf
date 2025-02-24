4353606b4977f5868c7f6278b34b54be1ce38970
pranav-8660 <pranavcse2003@gmail.com>
2025-02-24 12:45:11 +0530

    attached a policy i.e aws-s3-readonly-access-policy

--- aws-iam-terraform/awsiamforteraform.tf
+++ aws-iam-terraform/awsiamforteraform.tf
@@ -5,3 +5,8 @@ provider "aws" {
 resource "aws_iam_user" "developer" {
   name = "developer-user"
 }
+
+resource "aws_iam_user_policy_attachment" "developer_attach" {
+  user = aws_iam_user.developer.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
