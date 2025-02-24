3fef5b1e6dcace403b52c401cf9df1db4b7de5dc
pranav-8660 <pranavcse2003@gmail.com>
2025-02-24 13:03:50 +0530

    created iam role for ec2

--- aws-iam-terraform/awsiamforteraform.tf
+++ aws-iam-terraform/awsiamforteraform.tf
@@ -10,3 +10,22 @@ resource "aws_iam_user_policy_attachment" "developer_attach" {
   user = aws_iam_user.developer.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
+
+resource "aws_iam_role" "ec2_role" {
+  name = "EC2AccessRole"
+  assume_role_policy = <<EOF
+  {
+    "Version":"2012-10-17",
+    "Statement":[
+    {
+        "Effect":"Allow",
+        "Principal":{
+        "Service":"ec2.amazonaws.com"
+        },
+        "Action":"sts:AssumeRole"
+    }
+    ]
+  }
+  EOF
+
+}
