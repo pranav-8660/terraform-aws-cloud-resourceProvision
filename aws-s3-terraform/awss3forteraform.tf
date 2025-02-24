5b74eb5b4a779abfc882656422d5112924603243
pranav-8660 <pranavcse2003@gmail.com>
2025-02-24 15:05:36 +0530

    configured the terraform file to create an s3 bucket and to upload an image to that bucket

--- aws-s3-terraform/awss3forteraform.tf
+++ aws-s3-terraform/awss3forteraform.tf
@@ -1,3 +1,16 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+#create a bucket named as demo-test-bucket-pv8660
+resource "aws_s3_bucket" "pixar_bucket" {
+  bucket = "demo-test-bucket-pv8660"
+}
+
+#add some data(probably an image onto bucket created by terraform)
+
+resource "aws_s3_object" "upload_one" {
+  bucket = aws_s3_bucket.pixar_bucket.id
+  key = "randImg.png"
+  source = "/home/pranav/Pictures/Screenshots/randomimg.png"
+}
